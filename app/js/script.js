5ca19bc9639c9e119143bcdf2a1a363928c4a74c
jeptun <jeptun@gmail.com>
2022-08-25 22:09:24 +0200

    add better script

--- app/js/script.js
+++ app/js/script.js
@@ -10,18 +10,30 @@ fruits.map((fruit) => {
   console.log(fruit);
 });
 
-const acc = document.getElementsByClassName("accordion-button");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
-
+/**
+ * Accordion
+ */
+const faqManagement = () => {
+  const acc = document.getElementsByClassName("accordion-button");
+  let i;
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      let panel = this.nextElementSibling;
+      let closepanel = document.querySelectorAll(".panel");
+      panel.setAttribute(
+        "aria-expanded",
+        panel.getAttribute("aria-expanded") === "true" ? "false" : "true"
+      );
+      if (panel.getAttribute("aria-expanded") === "true") {
+        closepanel.forEach((p) => {
+          p.setAttribute("aria-expanded", "false");
+        });
+        panel.setAttribute("aria-expanded", "true");
+      } else {
+        panel.setAttribute("aria-expanded", "false");
+      }
+    });
+  }
+};
+faqManagement();
